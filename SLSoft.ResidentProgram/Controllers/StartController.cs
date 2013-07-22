d0f0a18fe6c34df73701cb17017deda12e4998d1
renwei <renwei151@163.com>
2013-07-22 10:35:47 +0800

    init commit
    init commit

--- SLSoft.ResidentProgram/Controllers/StartController.cs
+++ SLSoft.ResidentProgram/Controllers/StartController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SLSoft.EasyORM;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace SLSoft.ResidentProgram.Controllers
 {
@@ -13,8 +16,22 @@ namespace SLSoft.ResidentProgram.Controllers
 
         public ActionResult Index()
         {
+            DBFactory factory = new DBFactory();
+
+            string strdb = "";
+            AbstractDB db = factory.CreateDB("mysql");
+            DataTable dt = db.ExecSql("select * from slsoft_ias_sys_t_users");
+            if (dt.Rows.Count > 0)
+            {
+                foreach (DataRow row in dt.Rows)
+                {
+                    strdb += row["UserName"].ToString()+"     ";
+                }
+            }
+            ViewBag.strdb = strdb;
             return View();
         }
 
     }
 }
+
